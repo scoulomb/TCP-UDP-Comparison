654d55df46b9e1e49f3b0dc3a0ee9ad246a8bc50
Sylvain COULOMBEL <sylvaincoulombel@gmail.com>
2024-05-28 23:51:53 +0200

    initial commit - oriignal dojo

---
+++ code/a.py
@@ -0,0 +1,44 @@
+import socket
+import time
+from concurrent.futures import ThreadPoolExecutor
+
+
+def handle_connection(conn):
+    while True:
+        data = conn.recv(1024)
+        print("message: %s" % data)
+        time.sleep(1)
+        conn.sendall(b"thanks")
+        if data == b"stop":
+            conn.sendall(b"stop")
+            exit(0)
+
+def main_tcp():
+    # AF_INET == IPv4
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(('localhost', 7777))
+        s.listen()
+        while True:
+            conn, addr = s.accept()
+            POOL.submit(handle_connection, conn)
+
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.bind(('localhost', 7777))
+        while True:
+            data, addr = s.recvfrom(1024)
+            print("message: %s" % data)
+            time.sleep(1)
+            s.sendto(b"thanks", addr)
+            if data == b"stop":
+                s.sendto(b"stop", addr)
+                exit(0)
+
+
+if __name__ == "__main__":
+    print("I am server")
+    global POOL
+    POOL = ThreadPoolExecutor(max_workers=5)
+
+    # main()
+    main_tcp()
---
+++ code/c.py
@@ -0,0 +1,42 @@
+import random
+import socket
+import time
+
+def main_tcp():
+    with (socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s):
+        s.connect(("localhost", 7777))
+        s.sendall(b"from C")
+        while True:
+            data = s.recv(1024)
+            print("message: %s" % data)
+            time.sleep(1)
+            s.sendall(b"got something")
+            if random.Random().randint(0, 100) > 99:
+                s.sendall(b"stop")
+            if data == b"stop":
+                print("asked to stop, disregarding")
+                while True:
+                    s.sendall(b"live!")
+                    time.sleep(1)
+
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.sendto(b"from C", ("localhost", 7777))
+        while True:
+            data, addr = s.recvfrom(1024)
+            print("message: %s" % data)
+            time.sleep(1)
+            s.sendto(b"got something", addr)
+            if random.Random().randint(0, 100) > 80:
+                s.sendto(b"stop", addr)
+            if data == b"stop":
+                print("asked to stop, disregarding")
+                while True:
+                    s.sendto(b"live!", addr)
+                    time.sleep(1)
+
+
+if __name__ == "__main__":
+    print("I am a client")
+    #main()
+    main_tcp()
